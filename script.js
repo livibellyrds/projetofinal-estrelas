6d5fcf0abaa09a12a13088304bfcc2f1d0d4abe2
Danielle Rodrigues <livibellyrds@gmail.com>
2021-06-15 09:13:18 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,37 @@
+const imgPersonagens = document.getElementsByTagName('img');
+const nomePersonagens = document.getElementsByTagName('h2');
+const localizations = document.getElementsByTagName('p');
+
+var idsPersonagem = [];
+
+personagemAleatorio = () => {
+  for(i=1;i<=4;i++){
+   idsPersonagem.push(Math.floor(Math.random() * 671))
+  }
+};
+
+exibirPersonagem = () => {
+  personagemAleatorio();
+  return fetch(`https://rickandmortyapi.com/api/character/${idsPersonagem}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "content-type": "application/json"
+    }
+  }).then((response) => response.json()).then((data) => {
+    for(i=0;i<=3;i++) {
+      imgPersonagens[i].src = data[i].image;
+      nomePersonagens[i].innerHTML = data[i].name;
+      localizations[i].innerHTML = data[i].location.name
+    }
+  });
+}
+exibirPersonagem()
+
+
+
+
+
+
+
+
